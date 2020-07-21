4857003da2c6b89bd07807cf1aba75df6dba57df
Yeddcito <62890419+Yeddcito@users.noreply.github.com>
2020-07-20 20:39:49 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lol Bot</title>
+    </head>
+    <body style="background-color:black;">
+        <header>
+            <img align="center" width="800" height="150" src="imagenes/Logo_Lol.png"></img>
+            <h1 align="center" style="color:dimgrey;">LoL Bot</h1>
+        </header>
+    <div class="navbar" id='nav'>
+        <nav id="navegacion"> 
+            <ul  id="navegacion">
+              <a style="color:dimgrey;" href="http://localhost/Prueba/index.php">Inicio</a>
+              <a style="color:dimgrey;" href="http://localhost/Prueba/index.php">Cuenta</a>
+              <a style="color:dimgrey;" href="http://localhost/Prueba/tienda.php">Tienda</a>
+              <a style="color:dimgrey;" href="http://localhost/Prueba/index.php">Contacto</a>
+              ><a style="color:dimgrey;" href="http://localhost/Prueba/index.php">Preguntas Frecuentes</a>
+            </ul>
+        </nav>       
+    </div>
+        <div id="header2">
+                        <h2 align="center" style="color:dimgrey;">Facil Uso y Actualizaciones constantes</h2>
+        </div>
+        <div id="cuerpo">
+            <section aling="right">
+                <h3 style="color:dimgrey;">Caracteristicas de Lol Bot</h3>
+                <h4 style="color:dimgrey;">Proceso</h4>
+                <p style="color:dimgrey;">El Bot puede hacer la compra de RiotPoints (Moneda de pago de League of Legends) y el metodo de pago.</p>
+                <img height="100" width="100" src="imagenes/Rp.PNG"></img>
+                <img height="600" width="800" src="imagenes/PrecioRp.PNG"></img>
+                <h4 style="color:dimgrey;">Compra de items.</h4>
+                <p style="color:dimgrey;">Funcion de seleccionar el o los item que desea comprar</p>
+                
+                <img src="imagenes/ItemsLeague.PNG" alt=""/>
+            </section>
+            <p> </p>
+            <section id="compra">
+                <a aling="center" href="http://localhost/Prueba/tienda.php">
+                    <img src="imagenes/ComprarRp.PNG" alt=""/>
+                
+                
+                
+            </section>
+        </div>
+        <div id="columna">
+            <aside>
+              <nav>
+                <h1 style="color:dimgrey;">Sitios relacionados</h1>
+                <ul>
+                  <li><a style="color:dimgrey;" href="http://holi/">Discord</a></li>
+                  <li><a style="color:dimgrey;" href="http://www-yt">Canal YT</a></li>
+                  <li><a style="color:dimgrey;" href="http://sCd">LolRp Data Base</a></li>
+                  <li>...</li>
+                </ul>
+              </nav>
+            </aside>
+        </div>
+        <div id="pie pagina">
+            <footer>
+                <h2 style="color:dimgrey;">Informacion</h2>
+                <li><p style="color:dimgrey;">LAHelper.</p></li>
+                <li><p style="color:dimgrey;"> Calle Teemo, Jonia , Runaterra</p></li>
+                <li><p style="color:dimgrey;">+56923657580</p></li>
+                <li><p style="color:dimgrey;">contacto@lahelper.com</p></li>
+            </footer>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
